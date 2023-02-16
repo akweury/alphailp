b84ae01d109f68ca706e89eb843b5eeca504c53d
jing <shajingyuan@me.com>
2023-02-16 16:23:11 +0100

    update logs

--- src/train.py
+++ src/train.py
@@ -193,7 +193,7 @@ def train_nsfr(args, NSFR, pm_prediction_dict, writer, rtpt, exp_output_path):
         loss_list.append(loss_i)
         rtpt.step(subtitle=f"loss={loss_i:2.2f}")
         writer.add_scalar("metric/train_loss", loss_i, global_step=epoch)
-        print("loss: ", loss_i)
+        print(f"(epoch {epoch}/{args.epochs - 1}) loss: ", loss_i)
 
         if epoch > 5 and loss_list[epoch - 1] - loss_list[epoch] < stopping_threshold:
             break
