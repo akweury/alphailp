7ffcb120d04120a7ad3317e1c344f543f544ac17
jing <shajingyuan@me.com>
2023-02-23 10:50:20 +0100

    update logs

--- src/log_utils.py
+++ src/log_utils.py
@@ -4,14 +4,14 @@ import datetime
 def create_log_file(exp_output_path):
     date_now = datetime.datetime.today().date()
     time_now = datetime.datetime.now().strftime("%H_%M_%S")
-    with open(str(exp_output_path / f"log_{date_now}_{time_now}.txt"), "w") as f:
+    file_name = str(exp_output_path / f"log_{date_now}_{time_now}.txt")
+    with open(file_name, "w") as f:
         f.write(f"Log from {date_now}, {time_now}")
 
-    return str(exp_output_path / "log.txt")
+    return str(exp_output_path / file_name)
 
 
 def add_lines(line_str, log_file):
     print(line_str)
     with open(log_file, "a") as f:
         f.write(line_str)
-
