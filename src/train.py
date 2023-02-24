d999e763b2cdbccc07ae458fe914395232598321
jing <shajingyuan@me.com>
2023-02-24 01:50:18 +0100

    fix a bug

--- src/train.py
+++ src/train.py
@@ -397,8 +397,8 @@ def train_and_eval(args, pm_prediction_dict, val_pos_loader, val_neg_loader, wri
 
     for i in range(args.min_beam, args.t_beam):
         # if generate new predicates, start the bs deep from 0
-        # if len(new_pi_clauses) > 0:
-        #     i = 2
+        if len(new_pi_clauses) > 0:
+            i = 2
         clause_generator, pi_clause_generator, FC = get_models(args, lang, val_pos_loader, val_neg_loader,
                                                                init_clauses, bk_clauses, pi_clauses, atoms, bk)
         # generate clauses # time-consuming code
