8dad1f408a427f5f1fb0b676dc56b6030f3e815a
jing <shajingyuan@me.com>
2023-02-27 15:09:15 +0100

    update strategy

--- src/train.py
+++ src/train.py
@@ -88,11 +88,11 @@ def get_args():
                         help="The accept threshold for sufficient clauses.")
     parser.add_argument("--uc_top", type=int, default=3,
                         help="The accept number for unclassified clauses.")
-    parser.add_argument("--uc_good_top", type=int, default=3,
+    parser.add_argument("--uc_good_top", type=int, default=5,
                         help="The accept number for unclassified good clauses.")
-    parser.add_argument("--sc_good_top", type=int, default=3,
+    parser.add_argument("--sc_good_top", type=int, default=20,
                         help="The accept number for sufficient good clauses.")
-    parser.add_argument("--nc_good_top", type=int, default=3,
+    parser.add_argument("--nc_good_top", type=int, default=20,
                         help="The accept number for necessary good clauses.")
     parser.add_argument("--n-data", type=float, default=200,
                         help="The number of data to be used.")
