8a0924bde5902d979a5dc62d1730b087511b885d
jing <shajingyuan@me.com>
2023-03-11 10:25:42 +0100

    update args

--- src/clause_generator.py
+++ src/clause_generator.py
@@ -703,7 +703,7 @@ class ClauseGenerator(object):
             else:
                 raise ValueError
         elif search_type == "sc":
-            if len(clause_dict["sc"]) > 0:
+            if len(clause_dict["sc"]) > 3:
                 refs += self.update_refs(clause_dict, args, priority="sc")
             else:
                 log_utils.add_lines(f'no sc for extension!', args.log_file)
