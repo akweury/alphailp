ce7200f47d893a1c3a33aec2c796bc800a6aeab1
jing <shajingyuan@me.com>
2023-03-12 12:50:17 +0100

    update dataset

--- src/data_hide.py
+++ src/data_hide.py
@@ -3,8 +3,8 @@ import percept
 
 
 def get_pred_res(args, data_type):
-    pos_dataset_folder = config.data_path / "hide" / args.dataset / data_type / 'true'
-    neg_dataset_folder = config.data_path / "hide" / args.dataset / data_type / 'false'
+    pos_dataset_folder = config.buffer_path / "hide" / args.dataset / data_type / 'true'
+    neg_dataset_folder = config.buffer_path / "hide" / args.dataset / data_type / 'false'
     pos_pred = percept.convert_data_to_tensor(args, pos_dataset_folder)
     neg_pred = percept.convert_data_to_tensor(args, neg_dataset_folder)
     if args.top_data < len(pos_pred):
