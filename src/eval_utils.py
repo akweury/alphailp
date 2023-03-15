1b96d71bc0f0e0ec48e296daf1baf2830b753ffa
shaji-tp <jingyuan.sha@tu-darmstadt.de>
2023-03-15 17:20:43 +0100

    update args

--- src/eval_utils.py
+++ src/eval_utils.py
@@ -12,28 +12,28 @@ def is_sn_th_good(score, data_size, threshold):
 
 
 def is_nc(score, data_size, threshold):
-    if score[1] + score[3] == data_size:
+    if score[1] + score[3] == score.sum():
         return True
     else:
         return False
 
 
 def is_nc_th_good(score, data_size, threshold):
-    if (score[1] + score[3]) / data_size > threshold:
+    if (score[1] + score[3]) / score.sum() > threshold:
         return True
     else:
         return False
 
 
 def is_sc(score, data_size, threshold):
-    if score[0] + score[1] == data_size and score[1] > 0:
+    if score[0] + score[1] == score.sum() and score[1] > 0:
         return True
     else:
         return False
 
 
 def is_sc_th_good(score, data_size, threshold):
-    if (score[0] + score[1]) / data_size > threshold and score[1] > 0:
+    if (score[0] + score[1]) / score.sum() > threshold and score[1] > 0:
         return True
     else:
         return False
@@ -47,7 +47,7 @@ def is_uc_th_good(score, threshold):
 
 
 def is_conflict(score, data_size, conflict_th):
-    if score[0] / data_size > conflict_th or score[2] / data_size > conflict_th:
+    if score[0] / score.sum() > conflict_th or score[2] / score.sum() > conflict_th:
         return True
     else:
         return False
