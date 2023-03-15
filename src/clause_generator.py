f413578b52ac416c3453021b26f17c8bd6369ed8
shaji-tp <jingyuan.sha@tu-darmstadt.de>
2023-03-15 11:32:31 +0100

    update args

--- src/clause_generator.py
+++ src/clause_generator.py
@@ -827,7 +827,7 @@ class PIClauseGenerator(object):
         uc_good_new_predicates = []
         nc_sc_new_predicates = []
         # cluster sufficient clauses
-        if len(beam_search_clauses['sc']) > 2:
+        if len(beam_search_clauses['sc']) > 1:
             sc_new_predicates, found_ns = self.cluster_invention(beam_search_clauses["sc"], pi_clauses,
                                                                  pos_pred.shape[0], args)
             log_utils.add_lines(f"new PI from sc: {len(sc_new_predicates)}\n", args.log_file)
