3ef71385d077367ab70688cc6c113d8c439f4f8a
jing <shajingyuan@me.com>
2023-03-19 23:03:03 +0100

    update full house

--- src/clause_generator.py
+++ src/clause_generator.py
@@ -1382,12 +1382,12 @@ class PIClauseGenerator(object):
             new_predicates += self.generate_new_predicate(sn_th_clu)
         if len(n_clu) > 0:
             new_predicates += self.generate_new_predicate(n_clu)
-        if len(nc_th_clu) > 0:
-            new_predicates += self.generate_new_predicate(nc_th_clu)[:5]
+        # if len(nc_th_clu) > 0:
+        #     new_predicates += self.generate_new_predicate(nc_th_clu)[:5]
         if len(s_clu) > 0:
             new_predicates += self.generate_new_predicate(s_clu)
-        if len(sc_th_clu) > 0:
-            new_predicates += self.generate_new_predicate(sc_th_clu)[:10]
+        # if len(sc_th_clu) > 0:
+        #     new_predicates += self.generate_new_predicate(sc_th_clu)[:10]
 
         return new_predicates, found_ns
 
