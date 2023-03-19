c25bb1b6eabd7634d9b6e2cc7695cdde0868c8e7
jing <shajingyuan@me.com>
2023-03-19 18:49:50 +0100

    update two pairs

--- src/clause_generator.py
+++ src/clause_generator.py
@@ -286,6 +286,8 @@ class ClauseGenerator(object):
             step = max_step
             refs = last_refs
 
+        if args.pi_top == 0:
+            step = max_step
         while step <= max_step:
 
             # log
