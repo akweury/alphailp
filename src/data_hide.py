dcca610cb452d30469eaf5027a9ec3ea78f1543f
jing <shajingyuan@me.com>
2023-04-02 16:32:01 +0200

    update cross

--- src/data_hide.py
+++ src/data_hide.py
@@ -7,10 +7,10 @@ def get_pred_res(args, data_type):
     pred_pos,pred_neg = percept.convert_data_to_tensor(args, od_res)
 
     # normalize the position
-    value_max = max(pred_pos[:, :, :3].max(), pred_neg[:, :, :3].max())
-    value_min = min(pred_pos[:, :, :3].min(), pred_neg[:, :, :3].min())
-    pred_pos[:, :, :3] = percept.normalization(pred_pos[:, :, :3], value_max, value_min)
-    pred_neg[:, :, :3] = percept.normalization(pred_neg[:, :, :3], value_max, value_min)
+    # value_max = max(pred_pos[:, :, :3].max(), pred_neg[:, :, :3].max())
+    # value_min = min(pred_pos[:, :, :3].min(), pred_neg[:, :, :3].min())
+    # pred_pos[:, :, :3] = percept.normalization(pred_pos[:, :, :3], value_max, value_min)
+    # pred_neg[:, :, :3] = percept.normalization(pred_neg[:, :, :3], value_max, value_min)
 
     if args.top_data < len(pred_pos):
         pred_pos = pred_pos[:args.top_data]
